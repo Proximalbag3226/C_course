a27487cb8f1e04abcb2ce38849ae1c653f3564a8
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-08-18 01:06:00 -0600

    Sentencia switch 2

--- IF/switch.c
+++ IF/switch.c
@@ -1,1 +1,38 @@
-//La sentencia switch la tenemos como una cantidad de elif pero para caeda caso corespondiente
+//La sentencia switch la tenemos como un condicional para multiples casos, lo cual seria lo ideal a utilizar en caso de tener mas de 3 casos 
+//A diferenncia de sus hermanos condicionales este solo aceptara por condicion un nunmero entero o un dato de tipo char 
+
+//Su sintaxis es parecida al sigueinte ejemplo 
+
+/*
+switch (selector){
+case etiqueta1: sentencia1; break; 
+case etiqueta2: sentencia2; break;
+case etiqueta3: sentencia3; break;
+case etiqueta4: sentencia4; break;
+default: sentencia;
+}
+*/
+
+//Ejemplo de uso 
+
+#include <stdio.h>
+
+int main(){
+    int numero;
+    printf("Ingrese un numero del 1 al 3: ");
+    scanf("%i", &numero);
+    switch (numero)
+    {
+    case 1:
+    printf("El numero precionado es 1");
+    break;
+    case 2:
+    printf("El numero precionado es 2 ");
+    break;
+    case 3:
+    printf("El numero precionado es 3 ");
+    break;
+    default: printf("Numero incorrecto");
+        break;
+    }
+}
