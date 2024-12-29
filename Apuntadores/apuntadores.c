a37287845a5f7b62489bf423a58e82b28c1489a4
Proximalbag3226 <perezcoronaluisenrique4@gmail.com>
2024-12-28 20:21:07 -0600

    Gauss-jordan para sistemas de 3x3

--- Apuntadores/apuntadores.c
+++ Apuntadores/apuntadores.c
@@ -54,4 +54,71 @@ p->edad;
 //De esta manera podemos acceder a los valores dentro de una estructura con una variable de tipo apuntador 
 
 
+//Ahora veremos los apuntadores a apuntadores 
+//Esto no es mas que un puntero que apunta a otro que a su vez apunta a un dato 
+//Su sintaxis se puede declarar de sla siguiente forma 
+int **z;
+
+//Usando doble asterisco podemos definir un doble apuntador y ¿para que podeoms utilizar los dobles apuntadores?
+//Primeramente se llegan a utilizar para el manejo y manipulacion de matrices dinamicas multimensionales 
+//Estasz matrices se caracterisan porque no tienen un tamaño estatico en el tiempo de compilacion si no mas bien este tamaño
+//Se define en tiempo de ejecucion, se utilizan punteros ya que no podemos definir el tamaño de momoria que vamos a utilizar 
+//Antes del tiempo de ejecucion, ya entrando en materia, los dobles punteros los podemos utilizar como una lista de listas
+//Es decir, nuestro pntero doble apunta a un arreglo de punteros, de esta manera cada puntero del arreglo apunta a una fila de la 
+//Matriz y esta fila a su vez es un arreglo dinamico de datos
+
+//La otra implementacion que podemos realizar de los dobles punteros es la de modificar el valor de un puntero dentro de una funcion 
+//Aqui un ejemplo de modificacion del valor de un puntero dentro de una funcion
+
+void modificarValor(int **p) {
+    **p = 10;
+}
+
+int main() {
+    int x = 5;
+    int *p = &x;
+    modificarValor(&p);
+    printf("El nuevo valor de x es: %d\n", x);
+    return 0;
+}
+
+//Como puedes ver, cuando modificamos el valor de un puntero dentro de una funcion, este cambio se vera reflejado en la variable original
+
+//Tambien podemos tener aputnadores constantes y apuntadores a constantes, el primero de ellos nos permite la modificacion del valor del apuntador 
+//Mas sin embargo no nos permite la modificacion de la direccion a la que apunta 
+
+//Ejemplo de declaracion de un apuntador constante 
+int main() {
+    int x = 5;
+
+    //Utilizamos la palabra reservada const para declara nuestro apuntador que va a almacenar el valor de x 
+    int *const p = &x;
+
+    //Aqui vamos a modificar el valor de x 
+    *p = 10;
+    printf("El nuevo valor de x es: %d\n", x);
+    return 0;
+}
+
+//En este ejemplo se puede ovbsercar que no hay ningun problema al cambiar los valores de la variable a la que apunta mas sin embargo 
+//Si intentamos modificar a donde apunta obtendriamos un error, es decir no podremos apuntar a una variable y que hayamos declarado
+
+//Ahora un ejemplo de apuntadores a constantes 
+
+int main() {
+
+    //Declaramos nuestras variables pero si obvervamos con detenimiento podemos darnos cuenta que a diferencia del codigo anterior 
+    //La palabra reservada const esta antes de la declaracion del tipo de variable lo que nos inidica que sera un valor constante 
+    //Por tanto la direccion de memoria puede ser modificada mas sin embargo el valor del apuntador no
+    int x = 5;
+    const int *p = &x;
+
+    //Aqui vamos a modificar el valor de x 
+    x = 10;
+    printf("El nuevo valor de x es: %d\n", x);
+    return 0;
+}
+
+//Ahora pasemos con los arreglos y la aritmetica de apuntadores
+
 
--- Gauss-jordan/gauss1_0.c
+++ Gauss-jordan/gauss1_0.c
@@ -1,45 +1,104 @@
 //Metodo de gauss-jordan para resolucion de sistemas de ecuaciones de 3x3 
 
-    #include <stdio.h>
-    #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-    void print_matrix(double matrix[2][3], int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 4; j++) {
-                printf("%.2f ", matrix[i][j]);
-            }
-            printf("\n");
+//Nuestra priera funcion se va a encargar de imprimir la matriz que reciba 
+void imprimir_matrix(double matrix[3][3]);
+
+//Nuestra segunda funcion se encargará de cargar los datos en la matriz y los valores independientes dentro de un vector de 3 dimensiones
+void cargar_datos(double matrix[3][3], double independientes[3]);
+
+//Nuestra tercera función se encargará de imprimir los resultados después de la resolución del sistema de ecuaciones
+void imprimir_resultados(double independientes[3]);
+
+//Nuestra cuarta función se encargará de resolver el sistema de ecuaciones utilizando el método de Gauss-Jordan
+void gauss_jordan(double matrix[3][3], double independientes[3]);
+
+//Todas nuestras funciones recibiran como argumentos una matriz de 3x3 y un vector de 3 dimensiones sea el caso que corresponda 
+
+
+//En el main tenemos ya la implementacion de nuestras funciones
+int main() {
+    double matrix[3][3];
+    double independientes[3];
+    cargar_datos(matrix, independientes);
+    printf("La matriz original es: \n");
+    imprimir_matrix(matrix);
+    printf("\n");
+    gauss_jordan(matrix, independientes);
+    return 0;
+}
+
+//Por medio de dos ciclos for vamos a ir imprimiendo la matriz por filas y columndas
+void imprimir_matrix(double matrix[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%.2f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+//Para cargar los datos
+void cargar_datos(double matrix[3][3], double independientes[3]){
+    printf("Ingrese los coeficientes de las ecuaciones:\n");
+
+    //Dentro del primer ciclo for se solicitaran los valores de los coeficinetes de las varibales por filas y columnas
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++){
+            printf("Ingrese los coeficientes de las varialbes: ");
+            scanf("%lf", &matrix[i][j]);
         }
     }
 
-    void cargar_datos(double matrix[2][3]){
-        printf("Ingrese los coeficientes de las ecuaciones:\n");
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; i < 3; j++){
-                printf("Ingrese los coeficientes de las varialbes y su igualacion: ");
-                scanf("%lf", &matrix[i][j]);
-            }
-        }
+    //Dentro del segundo ciclo for se solicitaran los valores de la variable independiente guardandonse en el vector de 3 dimensiones
+    for (int i = 0; i < 3; i++){
+        printf("Ingrese el valor de la variable independiente: ");
+        scanf("%lf", &independientes[i]);
     }
+}
 
-    void imprimir_resultados(double matrix[2][3]){
-        printf("\nResultados:\n");
-        printf("x = %.2f\ny = %.2f\nz = %.2f\n", matrix[0][3], matrix[1][3], matrix[2][3]);
-    }
+//Por medio de dos ciclos for vamos a imprimir los resultados que se van a almacenar en el vector de 3 dimensiones
+void imprimir_resultados(double independientes[3]){
+    printf("Resultados:\n");
+    printf("x = %.2f\ny = %.2f\nz = %.2f\n", independientes[0], independientes[1], independientes[2]);
+}
 
-    void gauss_jordan(double matrix[2][3]){
-        int j = 0;
-        for (int i = 0; i < 3; i++){
-            do
-            {
-                if (matrix[i][j] == 0) break;
-                else if (j == 2) {
-                    printf("La matriz no tiene solucion unica.\n");
-                    exit(1);
-                }
-                j++;
-            } while (/* condition */);
-            
+
+//Y la logica mas imporntate es la de la resolucion por Gauss-Jronda
+void gauss_jordan(double matrix[3][3], double independientes[3]){
+
+    //Vamos a volver todos los elementos de tipo matrix[i][i] 1 es decir la diagonal mayor tiene que ser igual a 1
+    for (int i = 0; i < 3; i++) {
+
+        //Para eso vamos a iterar sobre cada elemento de la diagonal
+        double pivote = matrix[i][i];
+        if (pivote == 0){
+            printf("La matriz no tiene solución única.");
+            return;
         }
         
+        //Vamos a volver 1 ese elemento, lo que en otros terminos se refiere a dividir toda esa entre el valor de nuestro elemento
+        //De la diagonal, incluido el valor del vector asignado a dicha fila 
+        for (int j = 0; j < 3; j++) {
+            matrix[i][j] /= pivote;
+        }
+        independientes[i] /= pivote;
+        
+    //Ahora vamos a eliminar los otros elementos de la columna de nuestro elemento de la diagonal lo que se refiere a realizar 
+    //Una multiplicacion y posteriormente una resta a los elementos de la columna
+    for (int j = 0; j < 3; j++) {
+        if (j!= i) {
+            double elemento = matrix[j][i];
+            for (int k = 0; k < 3; k++) {
+                matrix[j][k] -= matrix[i][k] * elemento;
+            }
+            independientes[j] -= independientes[i] * elemento;
+        }
     }
+    }
+    
+    //Finalmente tenemos que imprimir los resultados que estan almacenados en nuestro vector 
+    imprimir_resultados(independientes);
+}
