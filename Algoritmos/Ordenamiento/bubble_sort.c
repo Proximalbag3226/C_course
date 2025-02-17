df23388167d53080f25ced330a8ecd40661d17cd
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2025-02-16 22:45:20 -0600

    Bubble_sort

--- Algoritmos/Ordenamiento/bubble_sort.c
+++ Algoritmos/Ordenamiento/bubble_sort.c
@@ -1,1 +1,47 @@
 //Este es un algoritmo de ordenamiento que basa su funcionamiento en la comparacion de valores y el intercambio de estos mismos
+#include <stdio.h>
+
+// Función para intercambiar dos elementos
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    int swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; 
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1; 
+            }
+        }
+        if (swapped == 0)
+            break;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Arreglo original: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Arreglo ordenado: ");
+    printArray(arr, n);
+
+    return 0;
+}
